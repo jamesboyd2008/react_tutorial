f7e22df476f5a90439517953717765417dbff8b7
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-06-06 14:27:34 -0700

    Add colors experiment to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // print('testaroni jabroni');
 
 var operation = require('./calculator');
+var colors = require('colors');
 
 // console.log("Addition: " + operation['+'](5, 6));
 // console.log("Subtraction: " + operation['-'](5, 6));
@@ -9,8 +10,21 @@ var operation = require('./calculator');
 // console.log("Division: " + operation['/'](5, 6));
 
 // refactored:
-var mathWords = ["Addition", "Subtraction", "Multiplication", "Division"];
-
-Object.keys(operation).forEach(function(key, index){
-  console.log(mathWords[index] + ': ' + operation[key](5, 6));
-});
+// var mathWords = ["Addition", "Subtraction", "Multiplication", "Division"];
+//
+// Object.keys(operation).forEach(function(key, index){
+//   console.log(mathWords[index] + ': ' + operation[key](5, 6));
+// });
+console.log('\u{2192} node examples/normal-usage.js');
+console.log('First some yellow text'.yellow);
+console.log('Underline that text'.yellow.underline);
+console.log('Make it bold and red'.red.bold);
+console.log('Double Rainbows All Day Long'.rainbow);
+console.log('drop the base'.trap);
+console.log('drop the rainbow base'.trap.rainbow);
+console.log('Chains are also cool'.underline.red);
+console.log('America'.america);
+// console.log(''.);
+// console.log(''.);
+// console.log(''.);
+// console.log(''.);
