710fa8117ae7ecabaa28c9e039847de95332f6f1
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-06-06 12:59:25 -0700

    Update app.js to iterate over operation object

--- app.js
+++ app.js
@@ -3,7 +3,14 @@
 
 var operation = require('./calculator');
 
-console.log("Addition: " + operation['+'](5, 6));
-console.log("Subtraction: " + operation['-'](5, 6));
-console.log("Multiplication: " + operation['*'](5, 6));
-console.log("Division: " + operation['/'](5, 6));
+// console.log("Addition: " + operation['+'](5, 6));
+// console.log("Subtraction: " + operation['-'](5, 6));
+// console.log("Multiplication: " + operation['*'](5, 6));
+// console.log("Division: " + operation['/'](5, 6));
+
+// refactored:
+var mathWords = ["Addition", "Subtraction", "Multiplication", "Division"];
+
+Object.keys(operation).forEach(function(key, index){
+  console.log(mathWords[index] + ': ' + operation[key](5, 6));
+});
