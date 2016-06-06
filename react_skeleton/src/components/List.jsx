2db916dfd572e780e63386b971b348bdf93edea8
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-06-06 16:01:48 -0700

    Update index.html to render list items

--- react_skeleton/src/components/List.jsx
+++ react_skeleton/src/components/List.jsx
@@ -1,2 +1,18 @@
 var React = require('react');
 var ListItem = require('./Listitem.jsx');
+
+var ingredients = [{'id': 1, 'text': 'Eye of Newt'},
+                   {'id': 2, 'text': 'Stanky Leg'},
+                   {'id': 3, 'text': 'Turkey Leg'}];
+
+var List = React.createClass({
+  render: function() {
+    var listItems = ingredients.map(function(item){
+      return <ListItem key={item.id} ingredient={item.text}/>;
+    });
+
+    return (<ul>{listItems}</ul>);
+  }
+});
+
+module.exports = List;
