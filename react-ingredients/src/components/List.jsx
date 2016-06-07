9bdaa94e7d26439b273f6a1804d627113aee516b
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-06-06 19:31:29 -0700

    Add ListItem and List components

--- react-ingredients/src/components/List.jsx
+++ react-ingredients/src/components/List.jsx
@@ -1,18 +1,15 @@
 var React = require('react');
-var ListItem = require('./Listitem.jsx');
-
-var ingredients = [{'id': 1, 'text': 'Eye of Newt'},
-                   {'id': 2, 'text': 'Stanky Leg'},
-                   {'id': 3, 'text': 'Turkey Leg'}];
+var ListItem = require('./List.jsx');
 
 var List = React.createClass({
   render: function() {
-    var listItems = ingredients.map(function(item){
-      return <ListItem key={item.id} ingredient={item.text}/>;
-    });
 
-    return (<ul>{listItems}</ul>);
+    var createItem = function(text, index) {
+      return <ListItem key={index + text} text={text} />;
+    }
+
+    return (<ul>{this.props.items.map(createItem)}</ul>);
   }
-});
+})
 
 module.exports = List;
