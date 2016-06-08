9c1d71368924f75b1bbd3778db9a0b00596a8728
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-06-07 17:53:40 -0700

    Add bootstrap to ingredients project

---
+++ ingredients-styled/src/components/ListManager.jsx
@@ -0,0 +1,38 @@
+var React = require('react');
+var List = require('./List.jsx');
+
+var ListManager = React.createClass({
+  getInitialState: function() {
+    return ({items: [], newItemText: ''});
+  },
+  onChange: function(e) {
+    this.setState({newItemText: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    var currentItems = this.state.items;
+    currentItems.push(this.state.newItemText);
+    this.setState({items: currentItems, newItemText: ''});
+  },
+  render: function() {
+    return (
+      <div className='col-sm-4'>
+        <div className='panel panel-default'>
+          <div className='panel-heading'>
+            <h3>{this.props.title}</h3>
+          </div>
+          <div className='panel-body'>
+            <form onSubmit={this.handleSubmit}>
+              <input onChange={this.onChange} value={this.state.newItemText} />
+              <button>Add</button>
+            </form>
+            <List items={this.state.items} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+})
+
+module.exports = ListManager;
