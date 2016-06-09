489f63c6b00a9a9eb6b45ca2daa17c3ec44ddf41
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-06-09 12:46:20 -0700

    Add routes

---
+++ react-routing/src/components/Page1.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Page1 = React.createClass({
+  render: function() {
+    <h1>Dis here page 1, son.</h1>
+  }
+})
+
+module.exports = Page1;
---
+++ react-routing/src/components/Page2.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Page2 = React.createClass({
+  render: function() {
+    <h1>Dis here page 2, son.</h1>
+  }
+})
+
+module.exports = Page2;
