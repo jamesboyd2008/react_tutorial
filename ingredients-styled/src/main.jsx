f0448c9d6ed37d5cb0b29c7aa43f308419fe3c0a
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-06-08 19:20:26 -0700

    Add two more lists

--- ingredients-styled/src/main.jsx
+++ ingredients-styled/src/main.jsx
@@ -2,4 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var ListManager = require('./components/ListManager.jsx');
 
-ReactDOM.render(<ListManager title="These Ingredients:" />, document.getElementById('ingredients'));
+ReactDOM.render(<ListManager title="These Ingredientss:" />, document.getElementById('ingredients'));
+ReactDOM.render(<ListManager title="Foutons:" />, document.getElementById('foutons'));
+ReactDOM.render(<ListManager title="Open-Faced Sandwiches:" />, document.getElementById('sandwiches'));
