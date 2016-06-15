24d9709b6ae04c6cef7dc2657456c90674c09511
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-06-15 15:36:30 -0700

    Add dashboard component

---
+++ AwesomeProject/App/Components/Dashboard.js
@@ -0,0 +1,20 @@
+import React, {
+  Component
+}, from 'react';
+
+import {
+  Text,
+  View
+} from 'react-native';
+
+class Dashboard extends Component{
+  render() {
+    return (
+      <View>
+        <Text>Das Dashboard</Text>
+      </View>
+    )
+  }
+}
+
+module.exports = Dashboard;
