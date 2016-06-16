53cafbd04ec3dfd8d2efd038371176898dada1d8
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-06-15 18:23:10 -0700

    Add buttons

--- AwesomeProject/App/Components/Dashboard.js
+++ AwesomeProject/App/Components/Dashboard.js
@@ -3,8 +3,10 @@ import React, {
 } from 'react';
 
 import {
+  Image,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View
 } from 'react-native';
 
@@ -24,11 +26,46 @@ var styles = StyleSheet.create({
 });
 
 class Dashboard extends Component{
+  makeBackground(btn) {
+    var obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    }
+
+    if(btn === 0) {
+      obj.backgroundColor = '#48BBEC';
+    } else if(btn === 1) {
+      obj.backgroundColor = '#E77AAE';
+    } else {
+      obj.backgroundColor = '#758BF4';
+    }
+
+    return obj;
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Das Dashboard</Text>
-        <Text> {JSON.stringify(this.props.userInfo)} </Text>
+        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image} />
+        <TouchableHighlight
+          styles={this.makeBackground(0)}
+          onPress={this.goToProfile.bind(this)}
+          underlayColor='#88D4F5'>
+            <Text style={styles.buttonText}> View dis Profile </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          styles={this.makeBackground(1)}
+          onPress={this.goToRepos.bind(this)}
+          underlayColor='#88D4F5'>
+            <Text style={styles.buttonText}> View dis Profile </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+        styles={this.makeBackground(2)}
+          onPress={this.goToNotes.bind(this)}
+          underlayColor='#88D4F5'>
+            <Text style={styles.buttonText}> View dis Profile </Text>
+        </TouchableHighlight>
       </View>
     )
   }
